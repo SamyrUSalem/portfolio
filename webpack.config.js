313688c269689434665cdd21711011eb3463560e
SamyrUSalem <samyruchoasalem@hotmail.com>
2024-02-15 11:15:24 -0400

    Foi feito as configurações e instalações de dependencias adequadas para o webpack comprimir todos os arquivos css criados

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
-const path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
 
 module.exports = {
     entry: {
@@ -10,11 +13,24 @@ module.exports = {
     module: {
         rules: [{
             test: /\.css$/,
-            use: ["style-loader", "css-loader"]
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader' // Não é necessário definir minimize aqui
+            ]
         }]
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+            chunkFilename: "css/[id].css"
+        }),
+        new CopyWebpackPlugin({
+            patterns: [{ from: "./css", to: "css" }]
+        }),
+        new OptimizeCssAssetsPlugin() // Adiciona o plugin para otimizar os arquivos CSS
+    ],
     output: {
         path: path.resolve(__dirname, "public"),
         filename: "[name].bundle.min.js"
     }
-}
+};
